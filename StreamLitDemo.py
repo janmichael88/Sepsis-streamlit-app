af3d8406ac8f21c6716cd5d63ca8a44be4140979
Jan Michael Cayabyab Austria <55464895+janmichael88@users.noreply.github.com>
2020-10-07 15:30:00 -0700

    Update StreamLitDemo.py

--- StreamLitDemo.py
+++ StreamLitDemo.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import tensorflow as tf
 from keras.models import load_model
 import plotly.graph_objects as go
